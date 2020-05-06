ba90924f2bcf2fbb652a7ae9531238d2114f8e33
Hayden Sansum <hayden.e.sansum@gmail.com>
2020-05-06 12:06:46 -0400

    removing some comments

--- code/local/run_transformation_scripts.py
+++ code/local/run_transformation_scripts.py
@@ -2,13 +2,13 @@ from file_transformer import mp3_transformer
 from song_transform_functions import song_digitizer, song_downsampler
 
 # SAVE TO WAV
-# mpt = mp3_transformer('../../songs')
-# mpt.set_input_path('mp3')
-# mpt.set_output_path('wav')
-# mpt.set_overwrite(True)
+mpt = mp3_transformer('../../songs')
+mpt.set_input_path('mp3')
+mpt.set_output_path('wav')
+mpt.set_overwrite(True)
 
-# mpt.transform_song([song_downsampler, song_digitizer], mpt._input_folders, 'wav', 8000)
-# print("Completed transformation of songs to WAV")
+mpt.transform_song([song_downsampler, song_digitizer], mpt._input_folders, 'wav', 8000)
+print("Completed transformation of songs to WAV")
 
 
 # SAVE TO PICKLE

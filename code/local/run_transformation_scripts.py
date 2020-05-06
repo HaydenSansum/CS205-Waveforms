93f721305b6e3fe6db7b39206e9f5a14f4f718c8
Hayden Sansum <hayden.e.sansum@gmail.com>
2020-05-05 20:41:38 -0400

    completed the compression and put before the digitizing to speed up the process

--- code/local/run_transformation_scripts.py
+++ code/local/run_transformation_scripts.py
@@ -1,12 +1,12 @@
 from file_transformer import mp3_transformer
-from song_transform_functions import song_digitizer
+from song_transform_functions import song_digitizer, song_downsampler
 
 mpt = mp3_transformer('../../songs')
 mpt.set_input_path('mp3')
-mpt.set_output_path('test2')
+mpt.set_output_path('test-compress-digitize')
 mpt.set_overwrite(True)
 
-mpt.transform_song([song_digitizer], mpt._input_folders, 'mp3', 'wav', 44100)
+mpt.transform_song([song_downsampler, song_digitizer], mpt._input_folders, 'mp3', 'wav', 8000)
 print("Completed transformation of songs to WAV")
 
 

1e53df342b5650c519e1d5e705ff38aed4fbb587
Hayden Sansum <hayden.e.sansum@gmail.com>
2020-05-06 22:52:10 -0400

    adding full wavenet model code and implementing a trainign test script

--- code/local/run_model_training.py
+++ code/local/run_model_training.py
@@ -1,9 +1,14 @@
 from modelling_functions import gen_songs_from_pickle
+from wavenet import create_wavenet
+
 
 data_path = "../../songs/pickle/"
 
 song_train_data_generator = gen_songs_from_pickle(data_path, genres_to_train=None, shuffle=True, seed=205)
 
-for song in song_train_data_generator:
-    print(song)
+# for song in song_train_data_generator:
+#     print(song)
+
+wavenet_model = create_wavenet(1024, 256, [32, 32, 64, 128])
   
+print(wavenet_model.summary())

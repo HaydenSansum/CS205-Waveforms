ade0f838206cb40f6a5906f50b2e318c559c58bc
Hayden Sansum <hayden.e.sansum@gmail.com>
2020-05-07 11:39:03 -0400

    saving model outputs to file to test with

--- code/local/run_model_training.py
+++ code/local/run_model_training.py
@@ -4,6 +4,8 @@ from wavenet import create_wavenet
 import numpy as np
 
 data_path = "../../songs/pickle/"
+model_path = "../../models/"
+model_name = "basic_model"
 
 song_train_data_generator = gen_songs_from_pickle(data_path, data_size=1024, n_stride=256, n_channels=256, batch_size=128, genres_to_train=['Jazz'], shuffle=True, seed=205)
 
@@ -13,4 +15,10 @@ wavenet_model.compile(optimizer='adam', loss='categorical_crossentropy')
 
 wavenet_model.fit_generator(song_train_data_generator, epochs=1)
 
-print("finished")
+# Save model and weights out to disk
+wavenet_json = wavenet_model.to_json()
+with open(f"{model_path}{model_name}.json", "w") as save_model:
+    save_model.write(wavenet_json)
+
+wavenet_model.save_weights(f"{model_path}{model_name}.h5")
+print(f"Model Training Complete & Model saved to {model_path}")

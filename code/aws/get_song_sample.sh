117722d9223b736bfb8e79e948e9bfceda5a55b1
Hayden Sansum <hayden.e.sansum@gmail.com>
2020-05-08 19:53:59 -0400

    adding simple script to download sample of input data

---
+++ code/aws/get_song_sample.sh
@@ -0,0 +1,8 @@
+# Make a new folder and get a sample of raw songs from S3
+mkdir input_songs
+
+# Grab songs
+aws s3 sync s3://waveform-storage/input_data/song_mp3/Pop/sample_100 input_songs
+
+# Put into HDFS for processing
+hadoop fs -put input_songs

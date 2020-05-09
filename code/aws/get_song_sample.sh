4f68069ba4063e9c1c01f192f275c69530aa5d6e
Hayden Sansum <hayden.e.sansum@gmail.com>
2020-05-08 23:20:42 -0400

    some useful helper functions

--- code/aws/get_song_sample.sh
+++ code/aws/get_song_sample.sh
@@ -6,3 +6,9 @@ aws s3 sync s3://waveform-storage/input_data/song_mp3/Pop/sample_100 input_songs
 
 # Put into HDFS for processing
 hadoop fs -put input_songs
+
+# Run the processing code
+spark-submit spark_preprocessing.py
+
+# Read back from HDFS
+hadoop fs -get processed_songs
--- code/aws/spark_build_model.py
+++ code/aws/spark_build_model.py
@@ -7,16 +7,16 @@ import numpy as np
 import keras
 
 # =========== Parameters to Set ===========
-song_directory = "songs_binary_old/part-00000"
-model_save_out = "wavenet_v2"
+song_directory = "processedSongsHadoop/part-00000"
+model_save_out = "wavenet_big"
 
-num_nodes = 16
-n_data_partitions = 128
-data_size = 8192
-data_collect_stride = 4096
+num_nodes = 1
+n_data_partitions = 100
+data_size = 4096
+data_collect_stride = 2048
 
 stack_layers = 10
-num_stacks = 8
+num_stacks = 4
 
 n_filter_list = [32, 128, 256, 512]
 n_output_channels = 256
@@ -25,10 +25,11 @@ n_output_channels = 256
 assert (2**stack_layers)*num_stacks == data_size, "ERROR: Data size must match network size - (2^stack_layers) * num_stacks"  
 
 # =========== SPARK CONFIG ===========
-set_master_val = "local[" + str(num_nodes) + "]"
+# set_master_val = "local[" + str(num_nodes) + "]"
 
 from pyspark import SparkContext, SparkConf
-conf = SparkConf().setMaster(set_master_val).setAppName('Test')
+conf = SparkConf().setAppName('TrainWavenet').set("spark.driver.maxResultSize", "2G")
+# conf = SparkConf().setMaster(set_master_val).setAppName('TrainWavenet')
 sc = SparkContext(conf=conf)
 
 
@@ -36,11 +37,13 @@ sc = SparkContext(conf=conf)
 # s3_song_directory = "s3://waveform-storage/input_data/song_processed/Pop/part-00000"
 
 train_rdd = sc.pickleFile(song_directory, minPartitions=n_data_partitions) \
-                .flatMap(lambda x: split_song_to_train(x[1], data_size, data_collect_stride)) \
+                .flatMap(lambda x: split_song_to_train(x, data_size, data_collect_stride)) \
                 .map(lambda x: (x, one_hot_encode_chunk(x))) \
                 .map(lambda x: (np.array(x[0]).reshape(data_size,1), np.array(x[1])))
 
 print("Num Partitions: ", train_rdd.getNumPartitions())
+
+train_rdd.saveAsTextFile("checkHDFS.txt")
 
 # x_stack = []
 # y_stack = []
@@ -71,14 +74,9 @@ print(wavenet_model.summary())
 
 # ============ ELEPHAS TRAIN ===========
 spark_model = SparkModel(wavenet_model, mode='synchronous')
-spark_model.fit(train_rdd, epochs=1000, batch_size=32, verbose=1, validation_split=0.1)
+spark_model.fit(train_rdd, epochs=3, batch_size=32, verbose=1, validation_split=0.1)
 
 print("Finished Training :)")
-
-x_test = np.array(train_rdd.map(lambda x: x[0]).take(1))
-y_test = np.array(train_rdd.map(lambda x: x[1]).take(1))
-
-print("Final Loss = ", spark_model.master_network.evaluate(x_test, y_test, verbose=2))
 
 # =========== SAVE FITTED MDOEL ===========
 # Save model and weights out to local
@@ -90,5 +88,8 @@ with open(model_save_out + ".json", "w") as save_model:
     save_model.write(wavenet_json)
 
 
+x_test = np.array(train_rdd.map(lambda x: x[0]).take(1))
+y_test = np.array(train_rdd.map(lambda x: x[1]).take(1))
 
-print("Num Partitions: ", train_rdd.getNumPartitions())
+print("Final Loss = ", spark_model.master_network.evaluate(x_test, y_test, verbose=2))
+

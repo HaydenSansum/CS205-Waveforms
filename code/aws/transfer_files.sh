ceef3e510957dc96a636c7279c3ef6d258502b17
Hayden Sansum <hayden.e.sansum@gmail.com>
2020-05-10 19:16:40 -0400

    Update transfer_files.sh

--- code/aws/transfer_files.sh
+++ code/aws/transfer_files.sh
@@ -7,3 +7,4 @@ scp -i ~/.ssh/CS205-key.pem spark_model_building_functions.py $1:~
 scp -i ~/.ssh/CS205-key.pem spark_preprocessing_functions.py $1:~
 scp -i ~/.ssh/CS205-key.pem spark_preprocessing.py $1:~
 scp -i ~/.ssh/CS205-key.pem wavenet.py $1:~
+scp -i ~/.ssh/CS205-key.pem test_hdfs.sh
